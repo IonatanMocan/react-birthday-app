e130567de615495fd9bc908e5ac8e1348a465532
= <=>
2020-11-29 19:44:14 +0200

    fix the picture

--- src/data.js
+++ src/data.js
@@ -32,6 +32,6 @@ export default [
     name: 'Olga Stefanisina',
     age: 26,
     image:
-      'https://media-exp1.licdn.com/dms/image/C5603AQEHu8Xfkea4Hg/profile-displayphoto-shrink_200_200/0?e=1612396800&v=beta&t=qega3CDjXst_DRvlXFEIId9_MBksNguyNzdDPGsDWvA',
+      'https://images.gr-assets.com/users/1565730187p6/30423794.jpg',
   },
 ];
